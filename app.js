74964c22ec237822c288658aaa8b3698e183d709
Your GitHub Username <tadess1127@gmail.com>
2025-03-24 09:46:41 +0300

    I ahve add some css on landing and login

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //require("dotenv").config();
 const express = require("express")
 const app = express()
-const cors = require('cors')
+const cors = require("cors")
 app.use(cors())
 const usersRoutes = require("./routes/userRoutes")
 const questionRoutes = require("./routes/questionRoute")
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use("/api/users", usersRoutes)
 
 //question router
-app.use("/api/question",authMiddleWare, questionRoutes)
+app.use("/api/question", authMiddleWare, questionRoutes)
 
 async function start() {
   try {
