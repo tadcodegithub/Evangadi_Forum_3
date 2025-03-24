253b54fa3baec524d44130a3ddf412ccd1c91bb5
Tsegereda Bisrat <koki.abebe@gmail.com>
2025-03-23 21:07:08 -0400

    Added single question Api with database query

--- app.js
+++ app.js
@@ -4,7 +4,21 @@ const app = express();
 
 const questionRoutes = require("./routes/questionRoute");
 const authMiddleWare = require("./middleWare/authMiddleWare");
-PORT=5500
+const PORT=5500
+
+//register route
+app.post("/api/users/register",(req,res)=>{
+    res.send("resgister user")
+})
+//login user
+app.post("/api/users/login",(req,res)=>{
+    res.send("login user")
+})
+
+//check user
+app.get("/api/users/check",(req,res)=>{
+    res.send("check user")
+})
 
 //question router
 app.use("/api/question", questionRoutes);
